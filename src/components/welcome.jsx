f235f8390f154939bc70a1e6a611906569613401
Gabriel Martins Silva <gabriel.m.martins@ufv.br>
2019-11-21 13:18:33 -0300

    [add] welcome component

---
+++ src/components/welcome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default props => (
+    <div id="welcome-home" className="jumbotron text-center bg">
+        <br/>
+        <h1>Bem vindo ao meu Portfolio</h1>
+        <p id="about-me">Desenvolvido enquanto aprendo um pouco de React!</p>
+    </div>
+)
--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,12 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../template/custom.css'
 
-import React from 'react'
+import React, { Component } from 'react'
 
 import Navbar from '../template/Navbar'
+import Welcome from '../components/welcome'
 
-export default props => (
-    <div className="container">
-        <Navbar/>
-    </div>
-)
+export default class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <Navbar/>
+                <Welcome/>
+            </div>
+        )
+    }
+}
